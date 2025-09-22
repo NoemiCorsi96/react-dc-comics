7f489653931cc6ec1ef5723b1165cb205a3748fa
NoemiCorsi96 <noemicorsi96@gmail.com>
2025-09-22 19:23:17 +0200

    add 'import AppMain..'

--- react-comics/src/App.jsx
+++ react-comics/src/App.jsx
@@ -5,6 +5,7 @@ import logo from './assets/img/dc-logo.png'
 import AppHeader from './assets/components/AppHeader'
 import AppContent from './assets/components/AppContent'
 import AppBar from './assets/components/AppBar'
+import AppMain from './assets/components/AppMain'
 import AppFooter from './assets/components/AppFooter'
 
 function App() {
