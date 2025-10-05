d4302c43e88652bcb96b2f232187f1901c65b8b5
NoemiCorsi96 <noemicorsi96@gmail.com>
2025-10-05 13:18:57 +0200

    Added CSS styling for all components: header, main, footer, bar, and content

--- react-comics/src/assets/components/AppBar.jsx
+++ react-comics/src/assets/components/AppBar.jsx
@@ -1,13 +1,33 @@
+import comicIcon from '../img/buy-comics-digital-comics.png';
+import merchIcon from '../img/buy-comics-merchandise.png';
+import subIcon from '../img/buy-comics-subscriptions.png';
+import shopIcon from '../img/buy-comics-shop-locator.png';
+import visaIcon from '../img/buy-dc-power-visa.svg';
 export default function AppBar() {
     return (
         <>
             <section className='blueBar'>
                 <ul>
-                    <li>DIGITAL COMICS</li>
-                    <li>DC MERCHANDISE</li>
-                    <li>SUBSCRIPTION</li>
-                    <li>COMIC SHOP LOCATOR</li>
-                    <li>DC POWER VISA</li>
+                    <li>
+                        <img src={comicIcon} alt="Digital Comics" />
+                        <span>DIGITAL COMICS</span>
+                    </li>
+                    <li>
+                        <img src={merchIcon} alt="DC Merchandise" />
+                        <span>DC MERCHANDISE</span>
+                    </li>
+                    <li>
+                        <img src={subIcon} alt="Subscription" />
+                        <span>SUBSCRIPTION</span>
+                    </li>
+                    <li>
+                        <img src={shopIcon} alt="Comic Shop Locator" />
+                        <span>COMIC SHOP LOCATOR</span>
+                    </li>
+                    <li>
+                        <img src={visaIcon} alt="DC Power Visa" />
+                        <span>DC POWER VISA</span>
+                    </li>
                 </ul>
             </section>
         </>
--- react-comics/src/assets/components/AppFooter.jsx
+++ react-comics/src/assets/components/AppFooter.jsx
@@ -1,3 +1,8 @@
+import FaceIcon from '../img/footer-facebook.png';
+import TwitterIcon from '../img/footer-twitter.png';
+import TubeIcon from '../img/footer-youtube.png';
+import PinterestIcon from '../img/footer-pinterest.png';
+import PeriscopeIcon from '../img/footer-periscope.png';
 export default function AppFooter() {
     return (
         <>
@@ -5,7 +10,11 @@ export default function AppFooter() {
                 <button>SIGN-UP NOW!</button>
                 <div className="social">
                     <span>FOLLOW US</span>
-                    {/* Qui mettere le icone delle social img */}
+                    <img src={FaceIcon} alt="Facebook" />
+                    <img src={TwitterIcon} alt="Twitter" />
+                    <img src={TubeIcon} alt="YouTube" />
+                    <img src={PinterestIcon} alt="Pinterest" />
+                    <img src={PeriscopeIcon} alt="Periscope" />
                 </div>
             </footer>
         </>
