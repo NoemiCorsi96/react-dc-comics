4345fbf4f8e499970c2cada12ebefc3c5c14061c
NoemiCorsi96 <noemicorsi96@gmail.com>
2025-10-08 20:59:40 +0200

    Created 'menu' object array for the header navigation

---
+++ react-comics/src/data/header_menu.js
@@ -0,0 +1,58 @@
+const menu = [
+    {
+        id: 1,
+        text: 'CHARACTERS',
+        link: '#'
+    },
+    {
+        id: 2,
+        text: 'COMICS',
+        link: '#'
+    },
+    {
+        id: 3,
+        text: 'MOVIES',
+        link: '#'
+    },
+    {
+        id: 4,
+        text: 'TV',
+        link: '#'
+    },
+    {
+        id: 5,
+        text: 'GAMES',
+        link: '#'
+    },
+    {
+        id: 6,
+        text: 'COLLECTIBLES',
+        link: '#'
+    },
+    {
+        id: 7,
+        text: 'VIDEOS',
+        link: '#'
+    },
+    {
+        id: 8,
+        text: 'FANS',
+        link: '#'
+    },
+    {
+        id: 9,
+        text: 'FANS',
+        link: '#'
+    },
+    {
+        id: 10,
+        text: 'NEWS',
+        link: '#'
+    },
+    {
+        id: 11,
+        text: 'SHOP',
+        link: '#'
+    }
+]
+export default menu;
